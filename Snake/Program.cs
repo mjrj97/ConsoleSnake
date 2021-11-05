49dea42910141d916a2a9daa89322f95b6ac073d
mjrj97 <martin.jensen.1997@hotmail.com>
2021-11-05 12:00:22 +0100

    Bug fixes.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,37 +6,45 @@ namespace Snake
 {
     class Program
     {
-        static int sizeX = 16;
-        static int sizeY = 16;
+        // Size of the display
+        const int sizeX = 16;
+        const int sizeY = 16;
 
-        static List<char> presses = new List<char>();
-        static byte[,] grid = new byte[sizeX, sizeY];
+        // Game values
+        const int FLOOR = 1;
+        const int APPLE = 0;
+        const int FPS = 2;
 
-        static void Main(string[] args)
+        // List of presses since start
+        static readonly List<char> presses = new();
+
+        // Gameplay Grid
+        static readonly byte[,] grid = new byte[sizeX, sizeY];
+
+        // Random object
+        static readonly Random random = new();
+
+        static void Main()
         {
             DateTime lastTime = DateTime.Now;
-            for (int x = 0; x < sizeX; x++)
-            {
-                for (int y = 0; y < sizeY; y++)
-                {
-                    grid[x, y] = 1;
-                }
-            }
+            Clear();
 
-            grid[5, 3] = 0;
+            // Place apple
+            grid[5, 3] = APPLE;
 
+            // Place snake
             grid[2, 3] = 2;
             grid[3, 3] = 3;
 
             Console.CursorVisible = false;
-            presses.Add('d');
-            Thread keyLogger = new Thread(GetKeyPress);
+            Thread keyLogger = new(GetKeyPress);
             keyLogger.Start();
+            presses.Add('d');
 
-            ConsoleKeyInfo info = Console.ReadKey(false);
+            // Gameplay loop
             while (true)
             {
-                if ((DateTime.Now - lastTime) > TimeSpan.FromSeconds(0.5))
+                if ((DateTime.Now - lastTime) > TimeSpan.FromSeconds(1.0/((double)FPS)))
                 {
                     Move();
                     Render();
@@ -45,12 +53,13 @@ namespace Snake
             }
         }
 
-        public static void Move()
+        static void Move()
         {
+            // Find the head
             int headX = 0;
             int headY = 0;
 
-            int largestValue = 2;
+            int largestValue = FLOOR + 1;
 
             for (int x = 0; x < sizeX; x++)
             {
@@ -65,7 +74,8 @@ namespace Snake
                 }
             }
 
-            switch (presses[presses.Count-1])
+            // Use the last key press
+            switch (presses[^1])
             {
                 case 'w':
                     headY--;
@@ -81,15 +91,16 @@ namespace Snake
                     break;
             }
 
-            headX = headX % sizeX;
-            headY = headY % sizeY;
-
+            // Clamp the snake to the grid and make it appear on the other side if moving into wall
+            headX %= sizeX;
+            headY %= sizeY;
             if (headX < 0)
-                headX = headX + sizeX;
+                headX += sizeX;
             if (headY < 0)
-                headY = headY + sizeY;
+                headY += sizeY;
 
-            if (grid[headX,headY] == 0)
+            // Eat the apple or move the entire snake
+            if (grid[headX,headY] == APPLE)
             {
                 grid[headX, headY] = (byte)(largestValue + 1);
                 PlaceApple();
@@ -100,7 +111,7 @@ namespace Snake
                 {
                     for (int y = 0; y < sizeY; y++)
                     {
-                        if (grid[x, y] > 1)
+                        if (grid[x, y] > FLOOR)
                             grid[x, y]--;
                     }
                 }
@@ -108,12 +119,11 @@ namespace Snake
             }
         }
 
-        public static void PlaceApple()
+        static void PlaceApple()
         {
-            Random random = new Random();
             int x = random.Next(0, sizeX-1);
             int y = random.Next(0, sizeY-1);
-            if (grid[x,y] == 1)
+            if (grid[x,y] == FLOOR)
             {
                 grid[x, y] = 0;
             }
@@ -123,16 +133,16 @@ namespace Snake
             }
         }
 
-        public static void Render()
+        static void Render()
         {
             Console.SetCursorPosition(0, 0);
-            int highestValue = 0;
+            int head = 0;
             for (int x = 0; x < sizeX; x++)
             {
                 for (int y = 0; y < sizeY; y++)
                 {
-                    if (grid[x, y] > highestValue)
-                        highestValue = grid[x, y];
+                    if (grid[x, y] > head)
+                        head = grid[x, y];
                 }
             }
 
@@ -147,19 +157,19 @@ namespace Snake
                 Console.Write("|");
                 for (int x = 0; x < sizeY; x++)
                 {
-                    if (grid[x,y] == highestValue)
+                    if (grid[x,y] == head)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.Write("O ");
                         Console.ForegroundColor = ConsoleColor.White;
                     }
-                    else if (grid[x, y] > 1 && grid[x,y] != highestValue)
+                    else if (grid[x, y] > FLOOR)
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.Write("# ");
                         Console.ForegroundColor = ConsoleColor.White;
                     }
-                    else if (grid[x, y] == 0)
+                    else if (grid[x, y] == APPLE)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write("a ");
@@ -178,7 +188,18 @@ namespace Snake
             Console.ForegroundColor = ConsoleColor.Black;
         }
 
-        public static void GetKeyPress()
+        static void Clear()
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    grid[x, y] = FLOOR;
+                }
+            }
+        }
+
+        static void GetKeyPress()
         {
             while (true)
             {
